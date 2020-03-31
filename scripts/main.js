1b58c93376091ed9661bc73c4b5d0146a36e87be
imageny <62641690+imageny@users.noreply.github.com>
2020-03-30 22:02:34 -0500

    Added character limit, discovered API had a char limit

--- scripts/main.js
+++ scripts/main.js
@@ -9,3 +9,15 @@ function generate() {
   ttsAudio.load();
   ttsAudio.play();
 }
+
+function countChars(obj){
+    var maxLength = 128;
+    var strLength = obj.value.length;
+    var charRemain = (maxLength - strLength);
+    
+    if(charRemain < 0){
+        document.getElementById("charNum").innerHTML = '<span style="color: red;">You have exceeded the limit of '+maxLength+' characters</span>';
+    }else{
+        document.getElementById("charNum").innerHTML = charRemain+' characters remaining';
+    }
+}
